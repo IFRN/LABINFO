9708935131d4e1dfec3cca49b756da522d357ca8
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-04-19 13:14:36 -0300

    Update labinfo.sh

--- software/labinfo.sh
+++ software/labinfo.sh
@@ -17,6 +17,16 @@ echo "Type=Application" | sudo tee -a potigol_lancador.desktop
 echo "Exec=java -jar /usr/local/lib/potigol/epotigol.jar" | sudo tee -a potigol_lancador.desktop
 echo "Icon=/usr/local/lib/potigol/potigol.png" | sudo tee -a potigol_lancador.desktop
 sudo chmod +x potigol_lancador.desktop
+# Configurar bash
+echo "alias potigol='java -jar /usr/local/lib/potigol/potigol.jar'" | sudo tee -a /etc/bash.bashrc
+echo "alias epotigol='java -jar /usr/local/lib/potigol/epotigol.jar'" | sudo tee -a /etc/bash.bashrc
+
+# Antlr
+cd /usr/local/lib
+sudo curl -O http://www.antlr.org/download/antlr-4.7-complete.jar
+echo "export CLASSPATH='.:/usr/local/lib/antlr-4.7-complete.jar:$CLASSPATH'" | sudo tee -a /etc/bash.bashrc
+echo "alias antlr4='java -jar /usr/local/lib/antlr-4.7-complete.jar'"  | sudo tee -a /etc/bash.bashrc
+echo "alias grun='java org.antlr.v4.gui.TestRig'" | sudo tee -a /etc/bash.bashrc
 
 # Java 8
 sudo add-apt-repository ppa:webupd8team/java -y
@@ -33,5 +43,4 @@ sudo gem update
 sudo gem install gosu
 sudo gem2.3 update
 sudo gem2.3 install gosu
-
 
