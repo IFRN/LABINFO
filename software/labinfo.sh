4b90184d3756ee275656be07e0fda9016288ffab
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-04-19 15:50:34 -0300

    Update labinfo.sh

--- software/labinfo.sh
+++ software/labinfo.sh
@@ -53,3 +53,9 @@ sudo gem update
 sudo gem install gosu
 sudo gem2.3 update
 sudo gem2.3 install gosu
+
+# SBT
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt
