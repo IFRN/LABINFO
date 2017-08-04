d4ab6794efb939af2b226e69c10fb4f7f3d11a5d
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-08-04 16:20:26 -0300

    Atualizando ANTLR para a versão 4.7

--- software/Antlr4.sh
+++ software/Antlr4.sh
@@ -1,7 +1,7 @@
 # http://www.antlr.org/
 
 cd /usr/local/lib
-sudo wget http://www.antlr.org/download/antlr-4.5.3-complete.jar
-export CLASSPATH=".:/usr/local/lib/antlr-4.5.3-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /usr/local/lib/antlr-4.5.3-complete.jar'
+sudo wget http://www.antlr.org/download/antlr-4.7-complete.jar
+export CLASSPATH=".:/usr/local/lib/antlr-4.7-complete.jar:$CLASSPATH"
+alias antlr4='java -jar /usr/local/lib/antlr-4.7-complete.jar'
 alias grun='java org.antlr.v4.gui.TestRig'
