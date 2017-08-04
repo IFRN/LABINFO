a4ca79e7b3d6b65c4c2975372b515bbb852e813f
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-08-04 16:58:13 -0300

    Incluindo Scala

--- software/labinfo.sh
+++ software/labinfo.sh
@@ -57,6 +57,13 @@ sudo gem install gosu
 sudo gem2.3 update
 sudo gem2.3 install gosu
 
+# Scala
+sudo apt-get remove scala-library scala
+sudo wget http://scala-lang.org/files/archive/scala-2.12.3.deb
+sudo dpkg -i scala-2.12.3.deb
+sudo apt-get update
+sudo apt-get install scala
+
 # SBT
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
