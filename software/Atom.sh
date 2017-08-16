4e510d78cee1d2576187df1cc62c4cf39c7544b0
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-08-16 13:17:09 -0300

    Update Atom.sh

--- software/Atom.sh
+++ software/Atom.sh
@@ -2,3 +2,5 @@
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom
+sudo -l -u aluno apm install language-potigol
+sudo -l -u aluno apm install language-scala
