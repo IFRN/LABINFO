3a56a5807b2da0bd19e3af74ba46caf1050afb75
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-08-18 20:08:26 -0300

    Create potigol_atualizar.sh

---
+++ software/potigol_atualizar.sh
@@ -0,0 +1,11 @@
+# Atualização do Potigol
+su dgti
+sudo mkdir potigol
+cd potigol
+sudo wget https://github.com/potigol/Potigol/releases/download/0.9.12/potigol.zip
+sudo unzip -o *.zip
+sudo rm *.zip
+cd ..
+
+sudo rm -Rf /usr/local/lib/potigol
+sudo mv potigol /usr/local/lib/potigol
