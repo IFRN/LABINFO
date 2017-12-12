ed2116b2395f08061c96de42b454aea11d5513f4
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2017-12-12 11:26:09 -0300

    Update labinfo.sh

--- software/labinfo.sh
+++ software/labinfo.sh
@@ -28,9 +28,9 @@ echo "alias epotigol='java -jar /usr/local/lib/potigol/epotigol.jar'" | sudo tee
 
 # Antlr
 cd /usr/local/lib
-sudo curl -O http://www.antlr.org/download/antlr-4.7-complete.jar
-echo "export CLASSPATH='.:/usr/local/lib/antlr-4.7-complete.jar:$CLASSPATH'" | sudo tee -a /etc/bash.bashrc
-echo "alias antlr4='java -jar /usr/local/lib/antlr-4.7-complete.jar'"  | sudo tee -a /etc/bash.bashrc
+sudo curl -O http://www.antlr.org/download/antlr-4.7.1-complete.jar
+echo "export CLASSPATH='.:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH'" | sudo tee -a /etc/bash.bashrc
+echo "alias antlr4='java -jar /usr/local/lib/antlr-4.7.1-complete.jar'"  | sudo tee -a /etc/bash.bashrc
 echo "alias grun='java org.antlr.v4.gui.TestRig'" | sudo tee -a /etc/bash.bashrc
 
 # Java 8
