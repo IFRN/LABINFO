d644393939776625c3d23cff6b2ccd9c6f7673a6
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2018-05-25 18:52:16 -0300

    Update Antlr4.sh

--- software/Antlr4.sh
+++ software/Antlr4.sh
@@ -1,7 +1,7 @@
 # http://www.antlr.org/
 
 cd /usr/local/lib
-sudo wget http://www.antlr.org/download/antlr-4.7-complete.jar
-export CLASSPATH=".:/usr/local/lib/antlr-4.7-complete.jar:$CLASSPATH"
-alias antlr4='java -jar /usr/local/lib/antlr-4.7-complete.jar'
-alias grun='java org.antlr.v4.gui.TestRig'
+sudo curl -O http://www.antlr.org/download/antlr-4.7.1-complete.jar
+echo "export CLASSPATH='.:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH'" | sudo tee -a /etc/bash.bashrc
+echo "alias antlr4='java -jar /usr/local/lib/antlr-4.7.1-complete.jar'"  | sudo tee -a /etc/bash.bashrc
+echo "alias grun='java org.antlr.v4.gui.TestRig'" | sudo tee -a /etc/bash.bashrc
