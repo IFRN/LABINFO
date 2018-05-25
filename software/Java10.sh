b486ddbc19288a914495d420f52640ae992c3b08
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2018-05-25 18:49:58 -0300

    Update and rename Java9.sh to Java10.sh

---
+++ software/Java10.sh
@@ -0,0 +1,5 @@
+# http://tecadmin.net/install-oracle-java-8-jdk-8-ubuntu-via-ppa/
+
+sudo add-apt-repository ppa:linuxuprising/java -y
+sudo apt update
+sudo apt install oracle-java10-installer -y
--- software/Java9.sh
+++
@@ -1,5 +0,0 @@
-# http://tecadmin.net/install-oracle-java-8-jdk-8-ubuntu-via-ppa/
-
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo apt-get install oracle-java9-installer
