d7a73326de6a9c5ce718049e9fa43f89414d201e
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2018-05-25 18:51:03 -0300

    Update Scala.sh

--- software/Scala.sh
+++ software/Scala.sh
@@ -1,5 +1,5 @@
 sudo apt-get remove scala-library scala
-sudo wget http://scala-lang.org/files/archive/scala-2.12.3.deb
-sudo dpkg -i scala-2.12.3.deb
+sudo wget http://scala-lang.org/files/archive/scala-2.12.6.deb
+sudo dpkg -i scala-2.12.6.deb
 sudo apt-get update
 sudo apt-get install scala
