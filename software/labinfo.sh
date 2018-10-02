e0fa5040372b6cb0af027781c02856a441a18a48
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2018-10-02 11:21:36 -0300

    Update labinfo.sh

--- software/labinfo.sh
+++ software/labinfo.sh
@@ -63,8 +63,8 @@ sudo gem2.5 install gosu
 
 # Scala
 sudo apt-get remove scala-library scala
-sudo wget http://scala-lang.org/files/archive/scala-2.12.6.deb
-sudo dpkg -i scala-2.12.6.deb
+sudo wget http://scala-lang.org/files/archive/scala-2.12.7.deb
+sudo dpkg -i scala-2.12.7.deb
 sudo apt-get update
 sudo apt-get install scala
 
